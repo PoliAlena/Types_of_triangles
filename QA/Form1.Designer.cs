bf603aa1eb3f0b7107a209dcfefd1a4491d283ab
PoliAlena <128645010+PoliAlena@users.noreply.github.com>
2024-02-07 22:51:57 +0300

    Добавьте файлы проекта.

---
+++ QA/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace QA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.first = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.second = new System.Windows.Forms.TextBox();
+            this.third = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Font = new System.Drawing.Font("Cascadia Mono", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(422, 574);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // first
+            // 
+            this.first.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.first.Location = new System.Drawing.Point(283, 399);
+            this.first.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(148, 53);
+            this.first.TabIndex = 1;
+            this.first.TextChanged += new System.EventHandler(this.first_TextChanged);
+            this.first.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.first_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Mono", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(126, 232);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(883, 58);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите длины сторон треугольника";
+            // 
+            // second
+            // 
+            this.second.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.second.Location = new System.Drawing.Point(497, 399);
+            this.second.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(148, 53);
+            this.second.TabIndex = 3;
+            this.second.TextChanged += new System.EventHandler(this.second_TextChanged);
+            this.second.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.second_KeyPress);
+            // 
+            // third
+            // 
+            this.third.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.third.Location = new System.Drawing.Point(713, 399);
+            this.third.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.third.Name = "third";
+            this.third.Size = new System.Drawing.Size(148, 53);
+            this.third.TabIndex = 4;
+            this.third.TextChanged += new System.EventHandler(this.third_TextChanged);
+            this.third.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.third_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.Font = new System.Drawing.Font("Cascadia Mono", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(422, 574);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(310, 71);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Начать снова";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.third);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.first);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox first;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox second;
+        private System.Windows.Forms.TextBox third;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ QA/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace QA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(first.Text) || string.IsNullOrWhiteSpace(second.Text) || string.IsNullOrWhiteSpace(third.Text))
+            {
+                MessageBox.Show("Введите значения!!!");
+                return;
+            }
+            else
+            {
+                int a = Convert.ToInt32(first.Text);
+                int b = Convert.ToInt32(second.Text);
+                int c = Convert.ToInt32(third.Text);
+
+                if (a + b > c && b + c > a && c + a > b)
+                {
+                    first.Visible = false;
+                    second.Visible = false;
+                    third.Visible = false;
+                    button1.Visible = false;
+                    button2.Visible = true;
+
+                    if (a == b && b == c && c == a)
+                    {
+                        label1.Text = "Полученный тругольник равносторонний";
+                    }
+                    if (a != b && b != c && c != a)
+                    {
+                        label1.Text = "Полученный тругольник разносторонний";
+                    }
+                    if ((a == b) && a != c || (b == c) && b != a || (c == a) && c != b)
+                    {
+                        label1.Text = "Полученный тругольник равнобедренный";
+                    }
+
+                }
+                else
+                {
+                    label1.Text = "Такой треугольник не может существовать";
+                    first.Visible = false;
+                    second.Visible = false;
+                    third.Visible = false;
+                    button1.Visible = false;
+                    button2.Visible = true;
+                }
+            }
+        }
+
+        private void first_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Int(e);
+        }
+
+        private void second_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Int(e);
+        }
+
+        private void third_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Int(e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            first.Visible = true;
+            second.Visible = true;
+            third.Visible = true;
+            button1.Visible = true;
+            button2.Visible = false;
+            label1.Text = "Введите длины сторон треугольника";
+            first.Clear();
+            second.Clear();
+            third.Clear();
+        }
+
+        private void first_TextChanged(object sender, EventArgs e)
+        {
+            Null(first);
+        }
+
+        private void second_TextChanged(object sender, EventArgs e)
+        {
+            Null(second);
+        }
+
+        private void third_TextChanged(object sender, EventArgs e)
+        {
+            Null(third);
+        }
+
+        private void Null(System.Windows.Forms.TextBox text_b)
+        {
+            if (string.IsNullOrWhiteSpace(text_b.Text))
+            {
+                return;
+            }
+            if (Convert.ToInt32(text_b.Text) < 1)
+            {
+                MessageBox.Show("Значение не может быть равно 0");
+                text_b.Clear();
+                return;
+            }
+        }
+
+        private void Int(KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            { e.Handled = true; }
+            else return;
+        }
+    }
+}
